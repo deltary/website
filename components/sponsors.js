0eea2b8747a278c4600495e5ebd4e39865d8845b
Konsta Purtsi <kovipu@utu.fi>
2020-05-31 17:50:50 +0300

    Add link to contact page and update styles on Sponsors.

--- components/sponsors.js
+++ components/sponsors.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { DynamicLink } from '../lib/linkUtils';
 
 function Sponsors() {
   return (
     <div className="Sponsors">
       {/* TODO: fetch sponsors from WordPress */}
       <h1>Deltan tukena</h1>
-      <a target="_blank" href="https://www.loimu.fi/mika-on-loimu/miksi-kannattaa-liittya/liittymislomake_opiskelijat?source=delta_ry">
-        <img src="logo-loimu.png" />
-      </a>
+
+      <div className="Sponsors-logocontainer">
+        <a target="_blank" href="https://www.loimu.fi/mika-on-loimu/miksi-kannattaa-liittya/liittymislomake_opiskelijat?source=delta_ry">
+          <img src="logo-loimu.png" />
+        </a>
+      </div>
+
+      <DynamicLink as="/yrityksille/yhteistyohon-deltan-kanssa">
+        <a>Yhteistyöhön Deltan kanssa?</a>
+      </DynamicLink>
     </div>
   );
 }
