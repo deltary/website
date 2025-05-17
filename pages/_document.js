8f97653eb48e4b0cd15d11b25bf3653563c20a6f
Tuomas Ahola <taahol@utu.fi>
2025-05-17 18:46:24 +0300

    tags: drop Google tracking
    En valitettavasti usko voivani aivan hyvillä mielin ylläpitää
    tällaista seurantaa yhdistyksen verkkosivuilla.

--- pages/_document.js
+++ pages/_document.js
@@ -16,22 +16,6 @@ export default class MyDocument extends Document {
       <Html lang="fi">
         <Head>
           <Tags />
-          {isProduction && (
-            <Fragment>
-              <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
-              <script
-                dangerouslySetInnerHTML={{
-                  __html: `
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
-
-                  gtag('config', '${GA_TRACKING_ID}');
-                  `,
-                }}
-              />
-            </Fragment>
-          )}
         </Head>
         <body>
           <Main />
