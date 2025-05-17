7dcef731cf0d50e95186d00d91895beaf3acaa42
Tuomas Ahola <taahol@utu.fi>
2025-05-17 16:43:58 +0300

    navigation: drop javascript
    Pannaas nyt huvin ja urheilun vuoksi navigaatiolaatikko aukeamaan ja
    sulkeutumaan pelkällä HTML:llä ja CSS:llä.

--- components/header/header.js
+++ components/header/header.js
@@ -1,27 +1,19 @@
-import { useState } from 'react';
-
 import { Brand } from '../../components';
 import Navigation from './navigation';
 
 function Header({ navItems }) {
-  const [isMenuOpen, toggleOpen] = useState(false);
-
-  const openClass = isMenuOpen ? 'open' : '';
-
   return (
-    <header className={openClass}>
+    <header>
       <Brand isColorInverted={false} />
-      <div className="NavigationButton"
-           onClick={() => toggleOpen(!isMenuOpen)}>
-        <div id="nav-icon"
-             className={openClass}>
+      <div className="NavigationButton">
+        <a href="#nav" id="nav-icon-open"></a>
+        <a href="#" id="nav-icon-close"></a>
         <span></span>
         <span></span>
         <span></span>
         <span></span>
       </div>
-      </div>
-      <Navigation isOpen={isMenuOpen} navItems={navItems} closeNav={() => toggleOpen(false)}/>
+      <Navigation navItems={navItems} />
     </header>
   );
 }
