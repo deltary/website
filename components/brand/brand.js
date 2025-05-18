13cf5ccf3172626af0a66b2a630cd6e154e1ea4a
Tuomas Ahola <taahol@utu.fi>
2025-05-18 23:46:06 +0300

    brand: move SVG parameters to the file itself

--- components/brand/brand.js
+++ components/brand/brand.js
@@ -6,7 +6,7 @@ function Brand({ isColorInverted = false }) {
   return (
     <Link href="/">
       <a className={isColorInverted ? 'Brand inverted' : 'Brand'}>
-        <Logo fill="#330000" height="70px" width="120px" viewBox="100 -75 450 500" />
+        <Logo />
         <div>Turun yliopiston matemaattisten ja fysikaalisten tieteiden opiskelijoiden yhdistys Delta ry</div>
       </a>
     </Link>
